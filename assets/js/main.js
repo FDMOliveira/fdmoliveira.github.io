0f5a73842bb2624e0d9474507b514306c38c51eb
Filipe Oliveira <filipe.dmo@gmail.com>
2018-09-01 15:16:54 +0100

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@
         item.addEventListener("click", function() {                
             $('body')[0].style.overflow= "initial";
             document.getElementById("check").checked = false;
-            setTimeout(() => {
+            setTimeout(function () {
                 $("html, body").animate({
                     scrollTop: $('.'+item.dataset.name).offset().top - $('.topbar-container').height()
                 }, 700);
@@ -17,7 +17,7 @@
    })
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
-        siteContent.forEach((element,i) => {
+        siteContent.forEach(function (element,i) {
             if( $(element).offset().top + 100  < window.innerHeight + scroll) {
                 $(element).addClass('in');
                 siteContent.splice(i,1);
