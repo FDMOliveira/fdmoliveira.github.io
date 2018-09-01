4ec625909f8ceed996a719ce973edebf2140e419
Filipe Oliveira <filipe.dmo@gmail.com>
2018-09-01 23:58:06 +0100

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,43 @@
 (function () {
     $(function() {
         var siteContent = $.makeArray($('.article > .title-container > *, .article > .body > *:not(.projects), .projects > *' ));
-    $('.social img').each(function(index,item) {
-        $(item).addClass('in');
-    })
-    $('.menu-item').each(function(index,item) {
-        item.addEventListener("click", function() {                
-            $('body')[0].style.overflow= "initial";
-            document.getElementById("check").checked = false;
-            setTimeout(function () {
-                $("html, body").animate({
-                    scrollTop: $('.'+item.dataset.name).offset().top - $('.topbar-container').height()
-                }, 700);
-            }, 250);
+       $('.welcome').style.height= window.innerHeight - $('.topbar-container').style.height+"px";
+       setTimeout(function () {
+           $('.pic').classList.add('in');
+           $('.title').classList.add('in');
+       }, 1);
+        document.getElementById('check').addEventListener("click",function() {
+            if (document.getElementById('check').checked) {
+                $('body').style.overflow= "hidden";
+                addRemoveAnimation('menu-item', true);
+                addRemoveAnimation('menu-social-btn', true);
+            }    
+            else {
+                $('body').style.overflow= "initial";
+                addRemoveAnimation('menu-item', false);
+                addRemoveAnimation('menu-social-btn', false);
+            }
+       }); 
+       function addRemoveAnimation(querySelector, isAdd) {
+            Array.from(document.getElementsByClassName(querySelector)).forEach(function(item) {
+                if(isAdd)
+                    item.classList.add('anim');
+                else
+                    item.classList.remove('anim');
+            }) 
+        }
+        $('.social img').each(function(index,item) {
+            $(item).addClass('in');
+        })
+        $('.menu-item').each(function(index,item) {
+            item.addEventListener("click", function() {                
+                $('body')[0].style.overflow= "initial";
+                document.getElementById("check").checked = false;
+                setTimeout(function () {
+                    $("html, body").animate({
+                        scrollTop: $('.'+item.dataset.name).offset().top - $('.topbar-container').height()
+                    }, 700);
+                }, 250);
         })
    })
     $(window).scroll(function() {
