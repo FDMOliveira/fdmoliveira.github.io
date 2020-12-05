d2f36d68346a268ccb0ee4c9423964928180b2d5
Filipe Oliveira <filipe.oliveira@bluesoft.pt>
2020-12-05 17:37:48 +0000

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,44 @@
 (function () {
-        var siteContent = $.makeArray($('.article > .title-container > *, .article > .body > *:not(.projects):not(h2):not(h1), .projects > *' ));
-        document.querySelector('.welcome').style.height = window.innerHeight +"px";
-        setTimeout(function () {
-            $('.pic').addClass('in');
-            $('.welcome-text .title').addClass('in');
-        }, 1);
-        document.getElementById('check').addEventListener("click",function() {
-            if (document.getElementById('check').checked) {
-                $('body')[0].style.overflow= "hidden";
-                addRemoveAnimation('menu-item', true);
-                addRemoveAnimation('menu-social-btn', true);
-            }    
-            else {
-                $('body')[0].style.overflow= "initial";
-                addRemoveAnimation('menu-item', false);
-                addRemoveAnimation('menu-social-btn', false);
-            }
-       }); 
-       function addRemoveAnimation(querySelector, isAdd) {
-            $('.'+querySelector).each(function(index,item) {
-                if(isAdd)
-                    item.classList.add('anim');
-                else
-                    item.classList.remove('anim');
-            })
+    var siteContent = $.makeArray($('.article > .title-container > *, .article > .body > *:not(.projects):not(h2):not(h1), .projects > *' ));
+    document.querySelector('.welcome').style.height = window.innerHeight +"px";
+    setTimeout(function () {
+        $('.pic').addClass('in');
+        $('.welcome-text .title').addClass('in');
+    }, 0);
+    document.getElementById('check').addEventListener("click",function() {
+        if (document.getElementById('check').checked) {
+            $('body')[0].style.overflow= "hidden";
+            addRemoveAnimation('menu-item', true);
+            addRemoveAnimation('menu-social-btn', true);
+        }    
+        else {
+            $('body')[0].style.overflow= "initial";
+            addRemoveAnimation('menu-item', false);
+            addRemoveAnimation('menu-social-btn', false);
         }
-        $('.social img').each(function(index,item) {
-            $(item).addClass('in');
+    }); 
+    function addRemoveAnimation(querySelector, isAdd) {
+        $('.'+querySelector).each(function(index,item) {
+            if(isAdd)
+                item.classList.add('anim');
+            else
+                item.classList.remove('anim');
         })
-        $('.menu-item').each(function(index,item) {
-            $(item).on("click", function() {                
-                $('body')[0].style.overflow= "initial";
-                $("#check").prop('checked', false);
-                setTimeout(function () {
-                    $("html, body").animate({
-                        scrollTop: $('.'+item.dataset.name).offset().top - $('.topbar-container').height()
-                    }, 700);
-                }, 250);
+    }
+    $('.social img').each(function(index,item) {
+        $(item).addClass('in');
+    })
+    $('.menu-item').each(function(index,item) {
+        $(item).on("click", function() {                
+            $('body')[0].style.overflow= "initial";
+            $("#check").prop('checked', false);
+            setTimeout(function () {
+                $("html, body").animate({
+                    scrollTop: $('.'+item.dataset.name).offset().top - $('.topbar-container').height()
+                }, 700);
+            }, 250);
         })
-   })
+    })
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
         siteContent.forEach(function (element,i) {
